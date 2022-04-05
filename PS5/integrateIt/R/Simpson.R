9abdfc1719675b74042f0fa63c70b6801849ae63
amcharaniya <amcharaniya@gmail.com>
2022-04-05 10:56:52 -0500

    Updating with Class Definitions

---
+++ PS5/integrateIt/R/Simpson.R
@@ -0,0 +1,56 @@
+#' Simpson S4 Class
+#'
+#' @slot equation function.
+#' @slot x numeric.
+#' @slot y numeric.
+#' @slot x1 numeric.
+#' @slot y1 numeric.
+#' @slot n numeric.
+#' @slot intg numeric.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+setClass(
+  Class = "Simpson",
+  representation = representation(
+    fun = "function",
+    x = "numeric",
+    y = "numeric",
+    x1 = "numeric",
+    y1 = "numeric",
+    n = "numeric",
+    intg = "numeric"
+  ),
+  prototype = prototype(
+    fun = function(x) {
+      return(x)
+    },
+    x = NA_real_,
+    y = NA_real_,
+    x1 = NA_real_,
+    y1 = NA_real_,
+    n = NA_real_,
+    intg = NA_real_
+  )
+)
+
+setValidity("Simpson", function(object){
+  #here I'm just running one validity test to see if x and y are of equal length
+  if(length(object@x) != length(object@y)){
+    stop("@x and @y are not the same length")
+  }
+  }
+)
+
+#' @export
+setMethod(
+  "initialize", "Simpson",
+  function(.Object,...){
+    value <- callNextMethod()
+    validObject(value)
+    return(value)
+  }
+)
---
+++ PS5/integrateIt/R/Trapezoid.R
@@ -0,0 +1,55 @@
+#' Trapezoid S4 Class
+#'
+#' @slot equation function.
+#' @slot x numeric.
+#' @slot y numeric.
+#' @slot x1 numeric.
+#' @slot y1 numeric.
+#' @slot n numeric.
+#' @slot intg numeric.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+setClass(
+  Class = "Trapezoid",
+  representation = representation(
+    fun = "function",
+    x = "numeric",
+    y = "numeric",
+    x1 = "numeric",
+    y1 = "numeric",
+    n = "numeric",
+    intg = "numeric"
+  ),
+  prototype = prototype(
+    fun = function(x) {
+      return(x)
+    },
+    x = NA_real_,
+    y = NA_real_,
+    x1 = NA_real_,
+    y1 = NA_real_,
+    n = NA_real_,
+    intg = NA_real_
+  )
+)
+
+setValidity("Trapezoid", function(object){
+  #here I'm just running one validity test to see if x and y are of equal length
+  if(length(object@x) != length(object@y)){
+    stop("@x and @y are not the same length")
+  }
+    }
+)
+#' @export
+setMethod(
+  "initialize", "Trapezoid",
+  function(.Object,...){
+    value <- callNextMethod()
+    validObject(value)
+    return(value)
+  }
+)
